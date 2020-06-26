78118ec339854ec8d58ff0d198965600086f8a39
ashish-devv <akashish908@gmail.com>
2020-06-27 03:52:00 +0530

    working now

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,redirect,render_template,url_for,session,request
 import sqlite3
 import string
 import random
+import time
 
 
 app=Flask(__name__)
@@ -13,10 +14,22 @@ def randomno():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    session['uid']="ashish"
-    if "uid" in session:
-        return redirect(url_for('user'))
-    else:
+    if request.method=="GET":
+        if "uid" in session:
+            return redirect(url_for('user'))
+        else:
+            return render_template("index.html")
+    if request.method=="POST":
+        name=request.form['nm']
+        conn=sqlite3.connect('database.db')
+        c=conn.cursor()
+        unique=randomno()
+        s='INSERT INTO users (name,uniquelink)VALUES("'+str(name)+'","'+str(unique)+'")'
+        c.execute(s)
+        all=c.fetchone()
+        conn.commit()
+        c.close()
+        session['uid']=unique
         return render_template("index.html")
 
 @app.route('/user', methods=['GET', 'POST'])
@@ -49,12 +62,26 @@ def send(u):
         s='SELECT * from users WHERE uniquelink="'+str(u)+'"'
         c.execute(s)
         all=c.fetchone()
+        c.close()
         print(all)
         if all==None:
             return render_template("index.html")
         else:
             name=all[1]
-            return render_template("send_message.html",name=name)
+            uniq=all[2]
+            return render_template("send_message.html",name=name,uniq=uniq)
+    if request.method=="POST":
+        mssg=request.form['m']
+        conn=sqlite3.connect('database.db')
+        c=conn.cursor()
+        timeofmessage=time.ctime()
+        s='INSERT INTO messages (uniqueusers,msg,datetime)VALUES("'+str(u)+'","'+str(mssg)+'","'+timeofmessage+'")'
+        print(s)
+        c.execute(s)
+        all=c.fetchone()
+        conn.commit()
+        c.close()
+        return render_template("index.html")
 
 
 
